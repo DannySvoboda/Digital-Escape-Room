a5ceb3af86e2ebc577d7fae5399bdb7e798c2dea
DannySvoboda <155640918+DannySvoboda@users.noreply.github.com>
2024-09-05 02:54:26 +0000

    9/4/24 Update 2

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function layer1KEYDROP() {
-  const key = "CyBerSec"
+function checkCodeL1() {
+  const key = "cyber";
+  if (key == document.getElementById("Code").value.toLowerCase()){
+    document.getElementById("gate").style.display = "block";
+  }
+}
+
+function checkCodeL2() {
+  const key = "091506";
   if (key == document.getElementById("Code").value.toLowerCase()){
     document.getElementById("gate").style.display = "block";
   }
