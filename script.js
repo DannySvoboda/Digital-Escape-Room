f6b90617af805f2050db4a8e822d53e362be81ab
DannySvoboda <155640918+DannySvoboda@users.noreply.github.com>
2024-09-10 17:44:55 +0000

    9-10 edit

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function checkCodeL2() {
     document.getElementById("gate").style.display = "block";
   }
 }
+
+function checkCodeL3() {
+  let string = document.getElementById("Code").value;
+  console.log(string.length);
+  console.log(/[A-Z]/.test(string));
+  console.log(/\d/.test(string));
+  if (string.length >= 8 && /[A-Z]/.test(string) && /\d/.test(string)){
+    document.getElementById("gate").style.display = "block";
+  } else if (string.length < 8){
+    alert("Password must be at least 8 characters long");
+  } else if (/\d/.test(string) == false){
+    alert("Password must contain at least one number");
+  } else if (/[A-Z]/.test(string) == false){
+    alert("Password must contain at least one capital letter");
+  }
+}
